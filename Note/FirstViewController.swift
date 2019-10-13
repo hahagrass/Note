7331e9425609dc39e943f7b867406f52fb5b7b91
Eddy Chen <niiq103@hotmail.com>
2019-10-13 14:18:27 +0800

    Initial Commit

---
+++ Note/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Note
+//
+//  Created by Eddy Chen on 2019/10/13.
+//  Copyright © 2019 Eddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Note/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Note
+//
+//  Created by Eddy Chen on 2019/10/13.
+//  Copyright © 2019 Eddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
